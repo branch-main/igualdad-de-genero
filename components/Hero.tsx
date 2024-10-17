c3c007b38814a8140675883e84436158d50227d7
Alan <alan.roncal@tecsup.edu.pe>
2024-10-17 13:45:48 -0500

    refactor: change hero margins

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col sm:flex-row-reverse items-center mx-5 gap-10 mt-10 sm:mt-24 sm:mx-20">
+    <div className="flex flex-col sm:flex-row-reverse items-center mx-5 gap-16 sm:gap-10 mt-16 sm:mt-24 sm:mx-20">
       <Image
         className="sm:w-1/2"
         src={igualdadDeGenero}
         alt="Igualdad de género"
       />
-      <div className="sm:w-1/2 flex flex-col gap-5 items-center sm:items-start text-center sm:text-left">
-        <h1 className="font-bold text-6xl leading-normal">
+      <div className="sm:w-1/2 flex flex-col gap-10 sm:gap-5 items-center sm:items-start text-center sm:text-left">
+        <h1 className="font-bold text-6xl sm:leading-normal">
           <span className="text-red-500">Igualdad de género</span> en Tecsup
         </h1>
         <p className="font-medium text-gray-600 text-xl max-w-[80%]">
