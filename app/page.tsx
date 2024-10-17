f8ad8ea7d7f88232c9ab3f1d4300d1fac1b6a2dc
Alan <alan.roncal@tecsup.edu.pe>
2024-10-16 21:51:27 -0500

    fix data

--- app/page.tsx
+++ app/page.tsx
@@ -47,32 +47,38 @@ export default function Home() {
             <Chart
               text="¿Has presenciado o escuchado de situaciones de desigualdad de género en el instituto?"
               labels={["Sí", "No"]}
-              series={[100, 50]}
+              series={[7, 10]}
             />
             <Chart
               text="¿Sientes que tanto hombres como mujeres tienen las mismas oportunidades para participar en actividades extracurriculares o liderar proyectos en el instituto?"
               labels={["Sí", "No", "No estoy seguro/a"]}
-              series={[80, 70, 30]}
+              series={[11, 1, 6]}
             />
             <Chart
               text="¿Crees que el personal docente y administrativo del instituto trata a los estudiantes por igual sin importar su género?"
               labels={["Sí", "No"]}
-              series={[90, 60]}
+              series={[12, 6]}
             />
             <Chart
               text="¿Piensas que los materiales educativos (libros, videos, ejemplos en clase) son inclusivos y promueven la igualdad de género?"
               labels={["Sí", "No", "No estoy seguro/a"]}
-              series={[85, 65, 20]}
+              series={[8, 4, 6]}
             />
             <Chart
               text="¿Estás de acuerdo con que los reglamentos y políticas del instituto favorecen la igualdad de género?"
-              labels={["Sí", "No", "No estoy seguro/a"]}
-              series={[95, 55, 25]}
+              labels={[
+                "Totalmente de acuerdo",
+                "De acuerdo",
+                "Ni de acuerdo ni en desacuerdo",
+                "En desacuerdo",
+                "Totalmente en desacuerdo",
+              ]}
+              series={[7, 4, 6, 0, 1]}
             />
             <Chart
               text="¿Consideras que el instituto realiza suficientes campañas o actividades de concienciación sobre la igualdad de género?"
               labels={["Sí", "No"]}
-              series={[75, 85]}
+              series={[7, 11]}
             />
           </div>
         </div>
